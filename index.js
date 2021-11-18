e6c96467698b1da8db6357610ba73b38b29780fe
Amanur Rahman <59202695+Araman99@users.noreply.github.com>
2021-11-19 00:53:08 +0600

    done

--- index.js
+++ index.js
@@ -9,28 +9,27 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://aman:aman@cluster0.d2icu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d2icu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
     try {
         await client.connect();
-        const database = client.db("computer-networking");
-        const userCollection = database.collection("lab-report2");
+        const database = client.db(process.env.DB_NAME);
+        const userCollection = database.collection(process.env.DB_COLLECTION);
 
         console.log("db connection ok");
 
         // POST API 
-        // app.post('/users', async (req, res) => {
-        //     const newUser = req.body;
-        //     const result = await userCollection.insertOne(newUser);
-        //     console.log('Got new user', req.body);
-        //     console.log('Added user', result);
-        //     res.send("Hit the post");
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            const result = await userCollection.insertOne(newUser);
+            console.log('Got new user', req.body);
+            console.log('Added user', result);
+            res.send("Data posted successfully...");
+        })
+    }finally{
 
-        // })
-    } finally {
-        // await client.close();
     }
 }
 run().catch(console.dir);
